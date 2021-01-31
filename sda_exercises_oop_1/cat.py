c837f28675f49813e8f6ae4221d7a0c972021f8a
Patryq <p.kurc@o2.pl>
2021-01-31 13:00:28 +0100

    Created object cat

--- sda_exercises_oop_1/cat.py
+++ sda_exercises_oop_1/cat.py
@@ -2,7 +2,7 @@ class Cat:
     def __init__(self, name: str):
         self.name = name
 
-    def make_sound(self):
+    def make_sound(self) -> str:
         return f'Name is {self.name} sound: miau'
 
 
--- sda_exercises_oop_1/main.py
+++ sda_exercises_oop_1/main.py
@@ -1,7 +1,12 @@
+from sda_exercises_oop_1.cat import Cat
+
+
 def main():
-    print("Hello")
+    cat_object = Cat("Filemon")
+    sound = cat_object.make_sound()
+    print(sound)
+
 
 
 if __name__ == "__main__":
     main()
-
