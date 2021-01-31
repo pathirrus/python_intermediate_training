b70502458ab5be6a5c37c5bda79a98922708053a
Patryq <p.kurc@o2.pl>
2021-01-31 13:19:01 +0100

    Add sound atribute to cat

--- sda_exercises_oop_1/cat.py
+++ sda_exercises_oop_1/cat.py
@@ -1,9 +1,7 @@
 class Cat:
-    def __init__(self, name: str):
+    def __init__(self, name: str, sound="miau"):
         self.name = name
+        self.sound = sound
 
     def make_sound(self) -> str:
-        return f'Name is {self.name} sound: miau'
-
-
-
+        return f'Name is {self.name} sound: {self.sound}'
--- sda_exercises_oop_1/main.py
+++ sda_exercises_oop_1/main.py
@@ -3,9 +3,15 @@ from sda_exercises_oop_1.cat import Cat
 
 def main():
     cat_object = Cat("Filemon")
-    sound = cat_object.make_sound()
-    print(sound)
+    # sound = cat_object.make_sound()
+    cat_object2 = Cat("Bonifacy", "miałmiał")
+    cat_object3 = Cat("Kitek")
+    cat_object4 = Cat("Mruczek", "meow")
 
+    list_cats = [cat_object, cat_object2, cat_object3, cat_object4]
+    for cat in list_cats:
+        cat_sound = cat.make_sound()
+        print(cat_sound)
 
 
 if __name__ == "__main__":
