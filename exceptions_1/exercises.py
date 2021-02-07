a869f07eebf7d91a9cc19dc0c8f27e64157f5d9d
Patryq <p.kurc@o2.pl>
2021-02-07 09:59:42 +0100

    Exceptions error test

---
+++ exceptions_1/exercises.py
@@ -0,0 +1,8 @@
+def case_1():
+    list_of_numbers = [1, 2, 31, 54, 32]
+
+    print("Case_1 before")
+    result = list_of_numbers[5]
+    print("Case_1 after")
+
+
---
+++ exceptions_1/main.py
@@ -0,0 +1,13 @@
+from exceptions_1.exercises import case_1
+
+
+def main():
+    print('Start up')
+
+    case_1()
+
+    print("Finish")
+
+
+if __name__ == '__main__':
+    main()
